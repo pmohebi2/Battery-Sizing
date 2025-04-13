84abeb24d8d4a679d37391a8c10660118e6a3779
Parastoo Mohebi <147386466+pmohebi2@users.noreply.github.com>
2025-04-13 16:48:29 +0800

    Add files via upload

--- Main.py
+++ Main.py
@@ -7,35 +7,43 @@ from Output import save_and_plot_results
 
 # import data
 file_path = 'data.csv'
-Netload_values, Tou_price, battery_cost, roundtrip_efficiency, Rate, self_discharge, \
-Projectlife, charge_power_capacity, discharge_power_capacity, electricity_sell_price_ratio, CRF = initialize_data(file_path)
+Netload_values, Tou_price, battery_cost, roundtrip_efficiency, self_discharge, \
+ charge_power_capacity, discharge_power_capacity, electricity_sell_price_ratio, CRF, PD_rate = initialize_data(file_path)
 
 if __name__ == "__main__":
     # Import data
     file_path = 'data.csv'
-    Netload_values, Tou_price, battery_cost, roundtrip_efficiency, Rate, self_discharge, \
-    Projectlife, charge_power_capacity, discharge_power_capacity, electricity_sell_price_ratio, CRF = initialize_data(file_path)
+    Netload_values, Tou_price, battery_cost, roundtrip_efficiency, self_discharge, \
+    charge_power_capacity, discharge_power_capacity, electricity_sell_price_ratio, CRF, PD_rate = initialize_data(file_path)
 
     # Start time
     start_time = time.time()
 
     # Call the optimizer
     model = optimizer(Netload_values, Tou_price, battery_cost, roundtrip_efficiency, 
-                              Rate, self_discharge, Projectlife, charge_power_capacity, 
-                              discharge_power_capacity, electricity_sell_price_ratio, CRF)
+                               self_discharge, charge_power_capacity, discharge_power_capacity, electricity_sell_price_ratio, CRF, PD_rate)
 
     # End time
     end_time = time.time()
 
-    # Save results and plot
-    save_and_plot_results(model)
-
-    # Accessing the value of Max_Cap
-    max_cap_value = pyo.value(model.Max_Cap)
-
     # Calculate the total execution time
     execution_time = time.time() - start_time
 
-print("Optimal value of Max_Cap:", max_cap_value)
-print(f"Total execution time: {execution_time:.2f} seconds")
+    # Print objective function value, battery capacity, Grid_IMax values and running time
+    objective_value  = pyo.value(model.obj)
+    battery_capacity = pyo.value(model.Max_Cap)
+    Grid_IMax_values = [pyo.value(model.Grid_IMax[m]) for m in model.M]
+    
+    print(f"Objective Function Value: {objective_value:.1f} $")
+    print(f"Battery Capacity: {battery_capacity:.1f} kWh")
+    print("Grid_IMax Values:")
+    for value in Grid_IMax_values:
+        print(f"{value:.1f} kW")
+    print(f"Running Time: {execution_time:.2f} seconds")
+
+
+    # Save results and plot
+    save_and_plot_results(model)
+
+
 
